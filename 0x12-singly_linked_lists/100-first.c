6c607b2615b66b078b3468610bbcbe0dd0805ecd
MedBoss01 <aqlibous@gmail.com>
2023-04-28 17:42:25 -0700

    0x12 c advanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
